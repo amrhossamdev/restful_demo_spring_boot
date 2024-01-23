8ef3165a949efca99f361c87370501ef6dd0be66
Amr Hossam <amrhossam.dev@gmail.com>
2024-01-23 11:42:19 +0200

    Added global exception handling using @ControllerAdvice

--- src/main/java/com/amr/restapi/rest/StudentRestController.java
+++ src/main/java/com/amr/restapi/rest/StudentRestController.java
@@ -40,12 +40,5 @@ public class StudentRestController {
     }
 
     // add another generic exception handler
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(Exception exc) {
-        StudentErrorResponse error = new StudentErrorResponse();
-        error.setStatues(HttpStatus.BAD_REQUEST.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
-    }
+
 }
